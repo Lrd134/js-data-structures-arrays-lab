e55acb94ad4f92c23468bee122bc7ec428bb4a68
Lrd134 <larryc3200@gmail.com>
2021-08-27 19:59:48 -0400

    commit tree 46d1c4fe2f5aefd9d8a60e7f01c14b1b78a9bdbe

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+let prepend =  [ "Milo", "Otis", "Garfield" ];
+prepend.unshift("Odie");
+
+let append =  [ "Milo", "Otis", "Garfield" ];
+append.push("Odie");
+let removeFirst =  [ "Milo", "Otis", "Garfield" ];
+removeFirst.shift("Odie");
+let removeLast =  [ "Milo", "Otis", "Garfield" ];
+removeLast.pop("Odie");
